18f0ed39aebf4be22ffe1daaef12cbc5495c8474
Christian Rhodes <crhodes@jama.com>
2018-07-12 15:25:51 -0700

    001 - Updated with a test file called Random API that acts as a dummy POST request document sent from 3rd party

--- flask_service.py
+++ flask_service.py
@@ -4,33 +4,35 @@ app = Flask(__name__)
 
 
 #########################################################
-#                GET, POST, AND PUT
+#                   GET AND POST
 #                  REQUEST METHOD
 
 
-@app.route('/', methods=['GET', 'POST', 'PUT'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     jsonRequest = request.json
     if request.method == 'POST':
-        print("Got a post request!")
-        print(request)
-        print("middle!")
-        print('request.json: \t', jsonRequest)
-        print("end a post request!")
 
-        #g = json.loads(request.json)
+        print('Get a POST Request!: \t', request)
+        print('request.json: \t', jsonRequest)
+
+        j = json.loads(jsonRequest)
+
+        print("Manipulate the POST request!")
+
+        j[1]["first"] = "Fancy"
+        print(j)
+
+        print("END the POST Request!")
 
         print('form_data: ', request.form.get('api_source_code'))
-        # with open("post.json", "w") as f:
-        #     f.write(json.dumps(request.json))
 
         return "You are using POST"
 
-    elif request.method == 'PUT':
-        return "Why you using PUT"
-
     else:
-        return "You are probably using GET!"
+        with open("randomApi.txt","r") as f:
+            j = json.load(f)
+        return json.dumps(j)
 
 
 #########################################################
--- httpRequest.py
+++ httpRequest.py
@@ -1,8 +1,11 @@
 # importing the requests library
-import requests, json
+from flask_service import index
+import requests, json, os, re, glob
 
 # defining the api-endpoint
-API_ENDPOINT = "http://pastebin.com/api/api_post.php"
+
+#API_ENDPOINT = "https://api.github.com/repos/crhodes2/platform-samples/statuses/8fc89b210deae0214a5f10087d71ce18bbbe164f"
+API_ENDPOINT = "http://localhost:5000"
 
 # your API key here
 API_KEY = "1234567889abcdefxyz"
@@ -22,10 +25,18 @@ data = {'api_dev_key': API_KEY,
         'api_paste_code': source_code,
         'api_paste_format': 'python'}
 
-# sending post request and saving response as response object
-r = requests.post(url=API_ENDPOINT, data=data)
-#print(r.json())
+# getting get request and saving response as response object
+req = requests.get(url=API_ENDPOINT, data=data)
+jsonRequest = req.json()
+print(jsonRequest)
+
+jsonRequest[0]["first"] = "Galen"
+
+#sending post request and saving response as response object
+postReq = requests.post(url=API_ENDPOINT, json=json.dumps(jsonRequest))
+postReq_Info = postReq.text
+print("POST REQUEST IS --> %s" % postReq_Info)
 
 # extracting response text
-pastebin_url = r.text
-print("The pastebin URL is:%s" % pastebin_url)
+#pastebin_url = r.text
+#print("The pastebin URL is:%s" % pastebin_url)
