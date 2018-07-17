fbf52674d9a5a646f640b435377c442a2c7d4d77
Christian Rhodes <crhodes@jama.com>
2018-07-17 15:28:47 -0700

    Cleaning up Working Space

--- git_http_request.py
+++ git_http_request.py
@@ -4,13 +4,16 @@ import requests, json, os, re, glob
 
 # defining the api-endpoint
 
-#API_ENDPOINT = "http://localhost:5000"
-API_ENDPOINT = "https://api.github.com/repos/crhodes2/platform-samples/pulls/19"
-TO_POST = "http://localhost:5000/anotherAPI"
-
-# your API key here
+API_ENDPOINT = "https://api.github.com/repos/crhodes2/platform-samples/statuses/8da07da76b41230a67aae4a585f998b75a726f3b"
 API_KEY = "18a0c4df0757c68f13767c0568e1bfc66169c512"
+TO_POST = "http://localhost:5000/responseAPI"
 
+# API_ENDPOINT = "https://api.github.com/repos/crhodes2/platform-samples/pulls/19"
+# TO_POST = "http://localhost:5000/anotherAPI"
+#
+# # your API key here
+# API_KEY = "18a0c4df0757c68f13767c0568e1bfc66169c512"
+#
 # your source code here
 source_code = '''
 print("Hello, world!")
@@ -26,18 +29,20 @@ data = {'api_dev_key': API_KEY,
         'api_paste_code': source_code,
         'api_paste_format': 'python'}
 
-# getting get request and saving response as response object
-req = requests.get(url=API_ENDPOINT, data=data)
-jsonRequest = req.json()
-print(jsonRequest)
+authentication = auth=('crhodes2', '18a0c4df0757c68f13767c0568e1bfc66169c512')
+formattedAuth = ', '.join(authentication)
+# # # getting get request and saving response as response object
+# req = requests.post(url=API_ENDPOINT, data=data)
+# jsonRequest = req.json()
+# jsonRequestRead = ''.join(map(str, jsonRequest))
+#
+# # #sending post request and saving response as response object
+# postReq = requests.post(url=TO_POST, auth=('crhodes2', API_KEY), json=json.dumps(jsonRequestRead))
+# # postReq_Info = postReq.text
+# print("POST REQUEST IS --> %s" % postReq.text)
 
-# jsonRequest[0]["first"] = "Galen"
 
-#sending post request and saving response as response object
-postReq = requests.post(url=TO_POST, auth=('crhodes2', API_KEY), json=json.dumps(jsonRequest))
-postReq_Info = postReq.text
-print("POST REQUEST IS --> %s" % postReq_Info)
+headers = {'Authorization' : formattedAuth, 'Accept' : 'application/json', 'Content-Type' : 'application/json'}
+r = requests.post(TO_POST, headers=headers)
 
-# extracting response text
-#pastebin_url = r.text
-#print("The pastebin URL is:%s" % pastebin_url)
+print("POST REQUEST IS --> %s" % r.text)
